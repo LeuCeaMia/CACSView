6867d42bf3a9df56dd7021cb6e7df64411e8aaa7
LeuCeaMia <randomrelay@gmail.com>
2014-10-06 10:09:17 +0800

    The View so far

---
+++ src/unifiedview/UnifiedView.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package unifiedview;
+
+/**
+ *
+ * @author LCM
+ */
+public class UnifiedView {
+    
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        UniFrame uf = new UniFrame();
+        
+        uf.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
